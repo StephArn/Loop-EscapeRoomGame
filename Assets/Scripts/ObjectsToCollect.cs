265affdf495f378583e4feb217ca81c8c2ed9170
Vartic Mihai <mihai.vlad.vartic@gmail.com>
2022-01-15 15:46:35 +0200

    Added sounds

---
+++ Assets/Scripts/ObjectsToCollect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsToCollect : MonoBehaviour 
+{
+    public static int objects = 3;
+    // Use this for initialization
+    void Awake () 
+    {
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Player")
+            objects--;
+
+        //AudioSource audio = gameObject.addComponent<AudioSource >();
+        //audio.PlayOneShot((AudioClip)Assets.Load("shock.mp3"));
+
+        gameObject.SetActive(false);
+    }
+}
